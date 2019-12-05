b90a26d12eb2ccdaba00477e2bc627f2b2163177
Istvan Kocsis <istvan.kocsis@logmein.com>
2019-12-05 15:33:25 +0100

    file operations

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -9,20 +9,23 @@ template <typename T>
 class simple_vector
 {
 public:
-	const size_t element_size = sizeof(T);
+
+	static T end;
 
 	simple_vector()
 	{
-		buffer = (T*)malloc(16 * element_size);
+		buffer = (T*)malloc(16 * sizeof(T));
 		buffsize = 16;
 		size = 0;
 	}
 
-	T* resize_buffer(size_t new_size)
+	T* resize_buffer(size_t new_size) const
 	{
 		if (new_size > buffsize)
-			buffer = (T*)realloc(buffer, new_size * element_size);
-		
+		{
+			buffer = (T*)realloc(buffer, new_size * sizeof(T));
+			buffsize = new_size;
+		}
 		return buffer;
 	}
 
@@ -35,7 +38,7 @@ public:
 	simple_vector<T>& append(const T* data, size_t data_size)
 	{
 		resize_buffer(size + data_size);
-		memcpy(buffer + size, data, data_size * element_size);
+		memcpy(buffer + size, data, data_size * sizeof(T));
 		size += data_size;
 
 		return (*this);
@@ -54,7 +57,7 @@ public:
 	{
 		resize_buffer(size + other.size);
 
-		memcpy(buffer + size, other.buffer, other.size * element_size);
+		memcpy(buffer + size, other.buffer, other.size * sizeof(T));
 		size += other.size;
 
 		return (*this);
@@ -65,15 +68,23 @@ public:
 		size = 0;
 	}
 
+	T& operator[](size_t n)
+	{
+		if (n >= size)
+			return end;
+
+		return buffer[n];
+	}
+
 	simple_vector<T>& operator=(const simple_vector<T>& other)
 	{
 		if (&other == this)
 			return (*this);
 
-		buffer = (T*)malloc(other.size * element_size);
+		buffer = (T*)malloc(other.size * sizeof(T));
 		buffsize = other.size;
 
-		memcpy(buffer, other.buffer, other.size * element_size);
+		memcpy(buffer, other.buffer, other.size * sizeof(T));
 		size = other.size;
 
 		return (*this);
@@ -98,6 +109,11 @@ public:
 		return true;
 	}
 
+	bool operator!=(const simple_vector<T>& other)
+	{
+		return !(operator==(other));
+	}
+
 	simple_vector<T> substring(size_t from, size_t to = MAX_SIZE_T)
 	{
 		if (to > size)
@@ -106,10 +122,15 @@ public:
 		return { buffer + from, from - size };
 	}
 
+	size_t get_size()
+	{
+		return size;
+	}
+
 protected:
-	T* buffer;
-	size_t buffsize;
-	size_t size;
+	mutable T* buffer;
+	mutable size_t buffsize;
+	mutable size_t size;
 };
 
 // not null terminated
@@ -127,11 +148,21 @@ public:
 		append(text, l);
 	}
 
-	const char* c_str()
+	const char* c_str() const
 	{
 		resize_buffer(size + 1);
 		buffer[size] = 0;
 		return buffer;
+	}
+
+	size_t find(char c, size_t from = 0)
+	{
+		for (size_t i = from; i < size; ++i)
+		{
+			if (buffer[i] == c)
+				return i;
+		}
+		return MAX_SIZE_T;
 	}
 };
 
@@ -141,8 +172,22 @@ public:
 	node()
 	{}
 
+	static node end;
+
 	simple_string name;
 	simple_vector<node> childs;
+
+	node& get_element_by_name(const simple_string& element_name)
+	{
+		// TODO could use map or implement own hashtable search
+		for (size_t i = 0; i < childs.get_size(); ++i)
+		{
+			if (childs[i].name == element_name)
+				return childs[i];
+		}
+
+		return end;
+	}
 };
 
 class element_node : public node
@@ -186,23 +231,141 @@ public:
 	}
 };
 
+class config
+{
+public:
+	config()
+	{}
+
+	void load(const simple_string& file_name)
+	{
+		
+	}
+
+};
 
 
+class file_base
+{
+public:
+	file_base(const simple_string& file_name)
+	{
+		name = file_name;
+	}
+
+	virtual ~file_base()
+	{}
+
+	virtual simple_string read() = 0;
+	virtual bool wait_for_change() = 0;
+
+protected:
+	simple_string name;
+};
+
+class win32_file : public file_base
+{
+public:
+	win32_file(const simple_string& name) : file_base(name)
+	{}
+
+	~win32_file()
+	{}
+
+	simple_string read() override
+	{
+		const size_t buf_size = 5;
+		char buf[buf_size];
+		simple_string res;
+
+		HANDLE hFile = CreateFileA(name.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hFile == INVALID_HANDLE_VALUE)
+			return {};
+
+		while (true)
+		{
+			DWORD cnt = 0;
+			if (ReadFile(hFile, buf, buf_size, &cnt, NULL) == FALSE)
+			{
+				CloseHandle(hFile);
+				return {};
+			}
+
+			if (cnt < 0)
+			{
+				CloseHandle(hFile);
+				return {};
+			}
+
+			res.append(buf, cnt);
+
+			if (cnt < buf_size)
+			{
+				CloseHandle(hFile);
+				return res;
+			}
+		}
+		CloseHandle(hFile);
+		return {};
+	}
+
+	bool wait_for_change() override 
+	{
+		char full_path[MAX_PATH];
+
+		if (GetCurrentDirectoryA(MAX_PATH, full_path) == 0)
+			return false;
+
+		HANDLE h = FindFirstChangeNotificationA(full_path, FALSE, FILE_NOTIFY_CHANGE_LAST_WRITE);
+		if (h == INVALID_HANDLE_VALUE)
+		{
+			DWORD err = GetLastError();
+			return false;
+		}
+
+		if (WaitForSingleObjectEx(h, 1000, FALSE) == WAIT_OBJECT_0)
+		{
+			CloseHandle(h);
+			return true;
+		}
+		return false;
+	}
+};
 
 int main()
 {
 	root_node root{};
 
-	group_node group1 = root.append_child({ "GROUP1" });
-	group_node group2 = root.append_child({ "GROUP2" });
+	group_node group1("GROUP1");
+	group_node group2("GROUP2");
 
-	element_node element1 = group1.append_child({ "element1" });
-	element_node element2 = group1.append_child({ "element2" });
+	group1.append_child({ "element1" });
+	group1.append_child({ "element2" });
 
-	element_node element3 = group2.append_child({ "element3" });
-	element_node element4 = group2.append_child({ "element4" });
+	group2.append_child({ "element3" });
+	group2.append_child({ "element4" });
 
+	root.append_child(group1);
+	root.append_child(group2);
 
+	win32_file file("a.ini");
+	simple_string ini_data = file.read();
+
+	while (true)
+	{
+		bool res = file.wait_for_change();
+		if (res)
+		{
+			simple_string new_ini_data = file.read();
+			if (new_ini_data != ini_data)
+			{
+				//change
+
+			}
+		}
+	}
+
+	const char *d = ini_data.c_str();
 
 	return 0;
 }
